6a37e52b2c0f20f0901f1b90e2c2717ef85b3c9a
Nicolas Richard <NicolasRichard3@gmail.com>
2015-01-31 13:24:09 -0800

    got rid off useless columns

--- committee_name_cleanup_files/join.py
+++ committee_name_cleanup_files/join.py
@@ -6,10 +6,13 @@ print 'Start join'
 small_table = pd.read_csv('committee_names_unedited_excludes_candidate_control_committees.csv')
 big_table = pd.read_csv('funding_committees_includes_candidate_control.csv')
 
-# We don't need this (and causes a conflict bc the other table also has a column named like that.)
+# We don't need these for deduping
 small_table = small_table.drop('ID', axis=1)
+big_table = big_table.drop('ID', axis=1)
 
 # When we actually do the join
 joined_table = pd.merge(small_table, big_table, left_on='committee name', right_on='Name', how='left')
 
 print joined_table[:10]
+
+joined_table.to_csv('committee_names_with_all_columns.csv', index=False)
