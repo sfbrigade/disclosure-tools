5f80cc1308f55d04688ebc5966235cb500e84c00
Wenley Tong <wenley.tong@gmail.com>
2015-02-08 23:57:54 -0800

    Add initial server.
    Capable of serving a page with two records,
    receiving answer from the front-end,
    serving another pair of records.
    
    TODO: Randomize the records being served,
    store the answers somewhere,
    create consistent way of labeling records.

---
+++ dedupe_server/setup_deduper.py
@@ -0,0 +1,64 @@
+import csv
+import re
+import sys
+
+import dedupe
+from unidecode import unidecode
+
+# COPY-PASTED FROM csv_example.com
+def preProcess(column):
+    """
+    Do a little bit of data cleaning with the help of Unidecode and Regex.
+    Things like casing, extra spaces, quotes and new lines can be ignored.
+    """
+    import unidecode
+    column = column.decode("utf8")
+    column = unidecode.unidecode(column)
+    column = re.sub('  +', ' ', column)
+    column = re.sub('\n', ' ', column)
+    column = column.strip().strip('"').strip("'").lower().strip()
+    return column
+
+def readData(filename):
+    """
+    Read in our data from a CSV file and create a dictionary of records, 
+    where the key is a unique record ID and each value is dict
+    """
+
+    data_d = {}
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            clean_row = [(k, preProcess(v)) for (k, v) in row.items()]
+            row_id = int(row['Id'])
+            data_d[row_id] = dict(clean_row)
+
+    return data_d
+# END COPY-PASTE
+
+def setup_deduper():
+  input_file = sys.argv[1]
+  data_d = readData(input_file)
+
+  # THIS IS COPY-PASTED FROM csv_example.py
+  # Define the fields dedupe will pay attention to
+  #
+  # Notice how we are telling dedupe to use a custom field comparator
+  # for the 'Zip' field. 
+  fields = [
+      {'field' : 'Site name', 'type': 'String'},
+      {'field' : 'Address', 'type': 'String'},
+      {'field' : 'Zip', 'type': 'Exact', 'has missing' : True},
+      {'field' : 'Phone', 'type': 'String', 'has missing' : True},
+      ]
+
+  # Create a new deduper object and pass our data model to it.
+  deduper = dedupe.Dedupe(fields)
+
+  # To train dedupe, we feed it a sample of records.
+  deduper.sample(data_d, 150000)
+
+  # END OF COPY-PASTE
+
+  return deduper
+
---
+++ dedupe_server/test-server.py
@@ -0,0 +1,36 @@
+
+import os
+import sys
+import collections
+import logging
+import optparse
+from numpy import nan
+
+from bottle import route, run, jinja2_view, request, redirect
+
+# Local imports
+from setup_deduper import setup_deduper
+
+deduper = setup_deduper()
+uncertain_pairs = deduper.uncertainPairs()
+labels = {'distinct' : [], 'match' : []}
+
+fields = set(field[0] for field in deduper.data_model.field_comparators)
+
+@route('/run')
+@jinja2_view('run.html', template_lookup=['.', 'templates'])
+def dedupe_get():
+  label = ''
+  labeled = False
+
+  record_pair = uncertain_pairs[0]
+
+  return { 'record_pair': record_pair, 'fields': fields }
+
+@route('/run/submit', method='POST')
+def dedupe_submit():
+  print request.forms.get('answer')
+  redirect('/run')
+
+print 'starting server...'
+run(host='localhost', port=8080)
